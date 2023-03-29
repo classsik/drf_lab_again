28960befed708447d6308b4979bcb981c482e500
Azat <classsikcoder@gmail.com>
2023-03-29 13:15:12 +0300

    Fixes

--- main/urls.py
+++ main/urls.py
@@ -7,24 +7,14 @@ urlpatterns = [
     path('login', obtain_auth_token),
     path('logout', views.logout),
 
-    path('tours', views.get_tours),
-    path('tours/<int:pk>', views.get_tour),
-    path('tours/create', views.add_tour),
-    path('tours/<int:pk>/edit', views.edit_tour),
-    path('tours/<int:pk>/delete', views.delete_tour),
+    path('tours', views.add_get_tours),
+    path('tours/<int:pk>', views.get_edit_delete_tour),
 
-    path('excursions', views.get_excursions),
-    path('excursions/<int:pk>', views.get_excursion),
-    path('excursions/create', views.add_excursion),
-    path('excursions/<int:pk>/edit', views.edit_excursion),
-    path('excursions/<int:pk>/delete', views.delete_excursion),
+    path('excursions', views.add_get_excursions),
+    path('excursions/<int:pk>', views.get_edit_delete_excursion),
 
-    path('countries', views.get_countries),
-    path('countries/<int:pk>', views.get_country),
-    path('countries/create', views.add_country),
-    path('countries/<int:pk>/edit', views.edit_country),
-    path('countries/<int:pk>/delete', views.delete_country),
+    path('countries', views.add_get_countries),
+    path('countries/<int:pk>', views.get_edit_delete_country),
 
-    path('cart/<int:pk>', views.get_cart),
-    path('cart/<int:pk>', views.edit_cart),
+    path('cart/<int:pk>', views.get_edit_delete_cart),
 ]
--- main/views.py
+++ main/views.py
@@ -17,215 +17,187 @@ def logout(request):
             "message": "logout"
         }})
 
-
-@api_view(['GET'])
+@api_view(['GET', "POST"])
 @permission_classes((AllowAny,))
-def get_tours(_):
-    tours = Tour.objects.all()
-    serializer = TourSerializer(tours, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+def add_get_tours(request):
+    if request.method == 'GET':
+        tours = Tour.objects.all()
+        serializer = TourSerializer(tours, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        if request.user.is_superuser:
+            serializer = TourSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(status=status.HTTP_403_FORBIDDEN)
 
 
-@api_view(['GET'])
+@api_view(['PATCH', "GET", "DELETE"])
 @permission_classes((AllowAny,))
-def get_tour(_, pk):
-    try:
-        tour = Tour.objects.get(pk=pk)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+def get_edit_delete_tour(request, pk):
+    if request.method == 'GET':
+        try:
+            tour = Tour.objects.get(pk=pk)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    serializer = TourSerializer(tour)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer = TourSerializer(tour)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PATCH':
+        if request.user.is_superuser:
+            try:
+                tour = Tour.objects.get(pk=pk)
+            except:
+                return Response(status=status.HTTP_404_NOT_FOUND)
 
-
-@api_view(['POST'])
-@permission_classes((IsAdminUser,))
-def add_tour(request):
-    serializer = TourSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    else:
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['PATCH'])
-@permission_classes((IsAdminUser,))
-def edit_tour(request, pk):
-    try:
-        tour = Tour.objects.get(pk=pk)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    serializer = TourSerializer(data=request.data, instance=tour, partial=True)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    else:
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['DELETE'])
-@permission_classes((IsAdminUser,))
-def delete_tour(request, pk):
-    try:
-        tour = Tour.objects.get(pk=pk)
-        tour.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+            serializer = TourSerializer(data=request.data, instance=tour, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+    elif request.method == 'DELETE':
+        if request.user.is_superuser:
+            try:
+                tour = Tour.objects.get(pk=pk)
+                tour.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            except:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response(status=status.HTTP_403_FORBIDDEN)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PATCH', "DELETE"])
 @permission_classes((IsAuthenticated,))
-def get_cart(_, pk):
-    try:
-        cart = Cart.objects.get(pk=pk)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+def get_edit_delete_cart(request, pk):
+    if request.method == 'GET':
+        try:
+            cart = Cart.objects.get(pk=pk)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    serializer = CartSerializer(cart)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer = CartSerializer(cart)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PATCH':
+        try:
+            cart = Cart.objects.get(pk=pk)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-
-@api_view(['PATCH'])
-@permission_classes((IsAuthenticated,))
-def edit_cart(request, pk):
-    try:
-        cart = Cart.objects.get(pk=pk)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    serializer = CartSerializer(data=request.data, instance=cart, partial=True)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    else:
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['DELETE'])
-@permission_classes((IsAuthenticated,))
-def delete_cart(_, pk):
-    try:
-        cart = Cart.objects.get(pk=pk)
-        cart.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = CartSerializer(data=request.data, instance=cart, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        try:
+            cart = Cart.objects.get(pk=pk)
+            cart.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
 
-@api_view(['GET'])
+@api_view(['GET', "POST"])
 @permission_classes((IsAdminUser,))
-def get_excursions(_):
-    excursions = Exscursion.objects.all()
-    serializer = ExscursionSerializer(excursions, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+def add_get_excursions(request):
+    if request.method == 'GET':
+        excursions = Exscursion.objects.all()
+        serializer = ExscursionSerializer(excursions, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = ExscursionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PATCH', 'DELETE'])
 @permission_classes((IsAdminUser,))
-def get_excursion(_, pk):
-    try:
-        excursion = Exscursion.objects.get(pk=pk)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+def get_edit_delete_excursion(request, pk):
+    if request.method == 'GET':
+        try:
+            excursion = Exscursion.objects.get(pk=pk)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    serializer = ExscursionSerializer(excursion)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer = ExscursionSerializer(excursion)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PATCH':
+        try:
+            excursion = Exscursion.objects.get(pk=pk)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ExscursionSerializer(data=request.data, instance=excursion, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        try:
+            excursion = Exscursion.objects.get(pk=pk)
+            excursion.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
 
-@api_view(['POST'])
+@api_view(['GET', 'POST'])
 @permission_classes((IsAdminUser,))
-def add_excursion(request):
-    serializer = ExscursionSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    else:
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+def add_get_countries(request):
+    if request.method == 'GET':
+        countries = Country.objects.all()
+        serializer = CountrySerializer(countries, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = CountrySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['PATCH'])
+@api_view(['GET', "PATCH", "DELETE"])
 @permission_classes((IsAdminUser,))
-def edit_excursion(request, pk):
-    try:
-        excursion = Exscursion.objects.get(pk=pk)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+def get_edit_delete_country(request, pk):
+    if request.method == 'GET':
+        try:
+            country = Country.objects.get(pk=pk)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    serializer = ExscursionSerializer(data=request.data, instance=excursion, partial=True)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    else:
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer = CountrySerializer(country)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PATCH':
+        try:
+            country = Country.objects.get(pk=pk)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
+        serializer = CountrySerializer(data=request.data, instance=country, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        try:
+            country = Country.objects.get(pk=pk)
+            country.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-@api_view(['DELETE'])
-@permission_classes((IsAdminUser,))
-def delete_excursion(request, pk):
-    try:
-        excursion = Exscursion.objects.get(pk=pk)
-        excursion.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-
-@api_view(['GET'])
-@permission_classes((IsAdminUser,))
-def get_countries(_):
-    countries = Country.objects.all()
-    serializer = CountrySerializer(countries, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-@api_view(['GET'])
-@permission_classes((IsAdminUser,))
-def get_country(_, pk):
-    try:
-        country = Country.objects.get(pk=pk)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    serializer = CountrySerializer(country)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-@api_view(['POST'])
-@permission_classes((IsAdminUser,))
-def add_country(request):
-    serializer = CountrySerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    else:
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['PATCH'])
-@permission_classes((IsAdminUser,))
-def edit_country(request, pk):
-    try:
-        country = Country.objects.get(pk=pk)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    serializer = CountrySerializer(data=request.data, instance=country, partial=True)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    else:
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['DELETE'])
-@permission_classes((IsAdminUser,))
-def delete_country(request, pk):
-    try:
-        country = Country.objects.get(pk=pk)
-        country.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND)
